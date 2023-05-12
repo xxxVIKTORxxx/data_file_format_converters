881a11801e682bce9d3422630441a019c9a8e86b
xxxVIKTORxxx <109345462+xxxVIKTORxxx@users.noreply.github.com>
2023-05-12 15:35:05 +0300

    Add files via upload

---
+++ excel_to_json.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+excel_data_df = pd.read_excel('data mess.xlsx', sheet_name='Sheet1')
+
+json_file = excel_data_df.to_json()
+
+print('Excel Sheet to JSON:\n', json_file)
+
+file = 'json_converted.json'
+with open(file, 'w') as to_convert:
+    to_convert.write(json_file)
+
+df = pd.read_json('json_converted.json')
+
+print(df)
+
